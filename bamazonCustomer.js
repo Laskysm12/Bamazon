f2187ad12f5b86874432457de67fd459e73655b9
Sam Lasky <Laskysm@gmail.com>
2018-03-28 21:38:45 -0400

    successful insertion of products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,14 +14,34 @@ var connection = mysql.createConnection({
 
 // Connection to the MySQL server and SQL database
 connection.connect(function(error) {
-    if (error) 
-    throw error;
-    else loadProducts();
-    // Run the start function after the connection is made to prompt the user
-    // showProducts();
-    // console.log()
-});
+    if (error) throw error;
+    console.log("connected as id " + connection.threadId);
 
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+      });
+      connection.end();
+    });
+
+    // Run the start function after the connection is made to prompt the user
+    // loadProducts();
+    // console.log()
+
+// Function to load the products table from the database and print results to the console
+// function loadProducts() {
+//     // Selects all of the data from the MySQL products table
+//     connection.query("SELECT * FROM products", function(err, res) {
+//       if (err) throw err;
+//       console.log
+  
+//       // Draw the table in the terminal using the response
+//     //   console.table(res);
+  
+//       // Then prompt the customer for their choice of product, pass all the products to promptCustomerForItem
+//     //   promptCustomerForItem(res);
+//     });
+//   }
 
 // function start() {
 //     inquirer
